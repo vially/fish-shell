2eeae308459e8b6277ecc45c46004a48fa150a0b
ridiculousfish <corydoras@ridiculousfish.com>
2015-10-30 16:57:13 -0700

    Migrate psub to docopt

--- share/functions/psub.fish
+++ share/functions/psub.fish
@@ -1,44 +1,18 @@
 
-function psub --description "Read from stdin into a file and output the filename. Remove the file when the command that called psub exits."
-
+function psub --description "Read from stdin into a file and output the filename. Remove the file when the command that called psub exits." --signature '
+  Usage: psub [-f | --file]
+	     psub [-h | --help]
+  Options:
+    -f, --file  Communicate using a regular file, not a named pipe
+	-h, --help  Display help and exit
+'
 	set -l filename
 	set -l funcname
-	set -l use_fifo 1
-	set -l shortopt -o hf
-	set -l longopt -l help,file
-
-	if getopt -T >/dev/null
-		set longopt
+	if set -lq opt_help
+		__fish_print_help psub
+		return 0
 	end
-
-	if not getopt -n psub -Q $shortopt $longopt -- $argv >/dev/null
-		return 1
-	end
-
-	set -l tmp (getopt $shortopt $longopt -- $argv)
-
-	eval set opt $tmp
-
-	while count $opt >/dev/null
-
-		switch $opt[1]
-			case -h --help
-				__fish_print_help psub
-				return 0
-
-			case -f --file
-				set use_fifo 0
-
-			case --
-				set -e opt[1]
-				break
-
-		end
-
-		set -e opt[1]
-
-	end
-
+	
 	if not status --is-command-substitution
 		echo psub: Not inside of command substitution >&2
 		return 1
@@ -49,7 +23,7 @@ function psub --description "Read from stdin into a file and output the filename
 		set TMPDIR /tmp
 	end
 
-	if test use_fifo = 1
+	if not set -lq opt_file
 		# Write output to pipe. This needs to be done in the background so
 		# that the command substitution exits without needing to wait for
 		# all the commands to exit
