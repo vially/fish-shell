87845c71dfef27b6bd2f2abde02f98e81ac655c4
ridiculousfish <corydoras@ridiculousfish.com>
2015-10-31 13:05:38 -0700

    Migrate fish_config to docopt

--- share/functions/fish_config.fish
+++ share/functions/fish_config.fish
@@ -1,4 +1,6 @@
-function fish_config --description "Launch fish's web based configuration"
+function fish_config --description "Launch fish's web based configuration" --signature '
+    Usage: fish_config [colors | prompt | functions | variables | history | bindings | abbreviations]
+'
 	# Support passing an initial tab like "colors" or "functions"
 	set -l initial_tab
 	if count $argv >/dev/null
